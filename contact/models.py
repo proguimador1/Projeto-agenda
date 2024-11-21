261af38781d7870e103f964b7bb8ac541cab078c
proguimador1 <guilherme.programmer1@gmail.com>
2024-11-21 18:07:17 -0300

    Added __str__ method to model 'Contact'

--- contact/models.py
+++ contact/models.py
@@ -9,3 +9,6 @@ class Contact(models.Model):
     e_mail = models.EmailField(max_length=50, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
