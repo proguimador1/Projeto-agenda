630f026b7a1d8d2d7d8d2a1c95d8fd5428250d10
proguimador1 <guilherme.programmer1@gmail.com>
2024-11-22 17:28:31 -0300

    Added more properties to 'Contact' model

--- contact/models.py
+++ contact/models.py
@@ -9,6 +9,8 @@ class Contact(models.Model):
     e_mail = models.EmailField(max_length=50, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m')
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
