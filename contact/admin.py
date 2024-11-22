ac016fc2b7807951d3769af6ca58e081b22fc2d7
proguimador1 <guilherme.programmer1@gmail.com>
2024-11-22 08:46:47 -0300

    Configured 'Contact' in django admin area

--- contact/admin.py
+++ contact/admin.py
@@ -5,4 +5,10 @@ from . import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    ...
+    list_display = ('id', 'first_name', 'last_name', 'phone',)
+    ordering = '-id',
+    search_fields = 'first_name', 'last_name', 'phone',
+    list_per_page = 15
+    list_max_show_all = 150
+    list_editable = 'first_name', 'last_name',
+    list_display_links = 'id',
