c68eb7d8195d0160e0b33b8e30b64a9d4486b539
proguimador1 <guilherme.programmer1@gmail.com>
2024-11-26 08:45:41 -0300

    Created model 'Category'

--- contact/admin.py
+++ contact/admin.py
@@ -12,3 +12,8 @@ class ContactAdmin(admin.ModelAdmin):
     list_max_show_all = 150
     list_editable = 'first_name', 'last_name',
     list_display_links = 'id',
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = '-id',
