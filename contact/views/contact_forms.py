ff9df099b5b6cfef221230c3bf0b5d308be6dd70
proguimador1 <guilherme.programmer1@gmail.com>
2025-01-02 14:35:39 -0300

    Edited contact_forms view

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,27 +1,6 @@
-from django.shortcuts import render, redirect
-from django.core.paginator import Paginator
-from django.core.exceptions import ValidationError
-from django.db.models import Q
-from ..models import Contact 
-from django import forms
+from django.shortcuts import render
+from ..forms import ContactForm
 
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model = Contact
-        fields = 'first_name', 'last_name', 'phone',
-
-    def clean(self):
-        cleaned_data = self.cleaned_data
-
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Error message',
-                code='Invalid'
-            )
-        )
-
-        return super().clean()
 
 def create(request):
     if request.method == 'POST':
